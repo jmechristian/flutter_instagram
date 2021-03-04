d2f1088317ea2c8c1a46412397bd251681e77acd
JMEChristian Digital <jmechristian@gmail.com>
2021-03-03 22:36:30 -0500

    login

---
+++ lib/screens/login/cubit/login_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_instagram/models/models.dart';
+import 'package:flutter_instagram/repositories/auth/auth_repository.dart';
+import 'package:meta/meta.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final AuthRepository _authRepository;
+  LoginCubit({@required AuthRepository authRepository})
+      : _authRepository = authRepository,
+        super(LoginState.initial());
+
+  void emailChanged(String value) {
+    emit(state.copyWith(email: value, status: LoginStatus.initial));
+  }
+
+  void passwordChanged(String value) {
+    emit(state.copyWith(password: value, status: LoginStatus.initial));
+  }
+
+  void loginWithCredentials() async {
+    if (!state.isFormValid || state.status == LoginStatus.submitting) return;
+    emit(state.copyWith(status: LoginStatus.submitting));
+    try {
+      await _authRepository.loginWithEmailandPassword(
+          email: state.email, password: state.password);
+      emit(state.copyWith(status: LoginStatus.success));
+    } on Failure catch (err) {
+      emit(state.copyWith(failure: err, status: LoginStatus.error));
+    }
+  }
+}
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_instagram/repositories/auth/auth_repository.dart';
+import 'package:flutter_instagram/screens/login/cubit/login_cubit.dart';
 
 class LoginScreen extends StatelessWidget {
   static const String routeName = '/login';
@@ -6,14 +9,124 @@ class LoginScreen extends StatelessWidget {
     return PageRouteBuilder(
       settings: const RouteSettings(name: routeName),
       transitionDuration: const Duration(seconds: 0),
-      pageBuilder: (_, __, ___) => LoginScreen(),
+      pageBuilder: (context, _, __) => BlocProvider<LoginCubit>(
+          create: (_) =>
+              LoginCubit(authRepository: context.read<AuthRepository>()),
+          child: LoginScreen()),
     );
   }
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Login Screen'),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: BlocConsumer<LoginCubit, LoginState>(
+          listener: (context, state) {
+            if (state.status == LoginStatus.error) {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text('Error'),
+                  content: Text(state.failure.message),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            return Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Card(
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            const Text(
+                              'Instagram',
+                              style: TextStyle(
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(
+                              height: 12.0,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(hintText: 'Email'),
+                              onChanged: (value) => context
+                                  .read<LoginCubit>()
+                                  .emailChanged(value),
+                              validator: (value) => !value.contains('@')
+                                  ? 'Please enter a valid email'
+                                  : null,
+                            ),
+                            const SizedBox(
+                              height: 16.0,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(hintText: 'Password'),
+                              obscureText: true,
+                              onChanged: (value) => context
+                                  .read<LoginCubit>()
+                                  .passwordChanged(value),
+                              validator: (value) => value.length < 6
+                                  ? 'Must be at least 6 characters'
+                                  : null,
+                            ),
+                            const SizedBox(
+                              height: 28.0,
+                            ),
+                            RaisedButton(
+                              elevation: 1,
+                              color: Theme.of(context).primaryColor,
+                              textColor: Colors.white,
+                              onPressed: () => _submitForm(
+                                context,
+                                state.status == LoginStatus.submitting,
+                              ),
+                              child: const Text('Log In'),
+                            ),
+                            const SizedBox(
+                              height: 12.0,
+                            ),
+                            RaisedButton(
+                              elevation: 1,
+                              color: Colors.grey[200],
+                              textColor: Colors.black,
+                              onPressed: () =>
+                                  print('Navigate to the Sign Up Screen'),
+                              child: const Text('No Account? Sign Up'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
+  }
+
+  void _submitForm(BuildContext context, bool isSubmitting) {
+    if (_formKey.currentState.validate() && !isSubmitting) {
+      context.read<LoginCubit>().loginWithCredentials();
+    }
   }
 }
